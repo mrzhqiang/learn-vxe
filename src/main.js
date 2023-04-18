64ccb73e2b3d2e7cf1807ccec19c626aa86fe942
mrzhqiang <287431404@qq.com>
2023-04-18 14:31:45 +0800

    import and use vxe-table

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
+
+Vue.config.productionTip = false;
+Vue.use(VXETable);
+// 给 vue 实例挂载内部对象，例如：
+// Vue.prototype.$XModal = VXETable.modal
+// Vue.prototype.$XPrint = VXETable.print
+// Vue.prototype.$XSaveFile = VXETable.saveFile
+// Vue.prototype.$XReadFile = VXETable.readFile
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
