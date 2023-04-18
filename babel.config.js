1b0f0b6635ed96907ee02d2a358eb23e16230f1a
mrzhqiang <287431404@qq.com>
2023-04-18 14:39:23 +0800

    demand loading

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "vxe-table",
+        "style": true // 样式是否也按需加载
+      }
+    ]
   ]
 }
